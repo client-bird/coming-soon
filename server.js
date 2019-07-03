e95661f9ae1a17692851f6e4b28ec2ddc5ad6377
Joerez <rezenbiz@gmail.com>
2019-07-02 22:16:16 -0700

    we're gonna rise from the fire

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3000
 const path = require('path') // research the path native node module
 const bodyParser = require('body-parser')
 
+const Email = require('./Email');
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.engine('hbs', exphbs({
@@ -21,8 +23,22 @@ require('./data/clientbird-comingsoon-db.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.post('/email', (req, res) => {
+  let email = new Email({email: req.body.email}).save((email) => {
+    res.render('index', {yay: 'Success!'});
+  })
+
+})
+
+app.get('/api/emails', (req, res) => {
+  Email.find({}).then((emails) => {
+    res.send(emails);
+  })
+})
 
 app.listen(process.env.PORT || port)
